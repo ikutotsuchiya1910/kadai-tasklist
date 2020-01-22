74f12ca1511d7d62444636e4877dd67cd8ed819b
ikuto.tsuchiya <ikutotsuchiya1910@gmail.com>
2020-01-22 17:03:17 +0000

    add bootstrap design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
+  get 'signup', to: 'users#new'
   resources :tasks
 end
 
