39a8668b4d0e4c5c018f2676e4678516a77635e0
ikuto.tsuchiya <ikutotsuchiya1910@gmail.com>
2020-01-23 04:47:56 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   root to: 'tasks#index'
-  
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-  get 'signup', to: 'users#new'
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
 end
 
